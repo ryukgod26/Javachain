c8a7274daf63ffe157c6500ab983ccd4fe36828d
raviwhitehat <126102004+raviwhitehat@users.noreply.github.com>
2025-08-04 21:11:12 +0530

    Refactor main method to initialize blockchain and output JSON representation

--- javachain.java
+++ javachain.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+
 public class javachain{
-    public static void main(String[] arga){
-      Block block1 = new Block("hash","secret");
-      Block block2 = new Block(block1.hash,"secret");
-      Block block3 = new Block(block2.hash,"top-secret");
-      Block block4 = new Block(block3.hash,"end");
 
-      System.out.println(block1.hash);
-      System.out.println(block2.hash);
-      System.out.println(block3.hash);
-      System.out.println(block4.hash);
+  public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static void main(String[] args){
+      
 
-    }
+        String blockchainJson =  new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
+		System.out.println(blockchainJson);
+	}
+
 }
+
